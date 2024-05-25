b6b950e6b344a611f54c08bf932fc94d243c168f
HamZa Samha <smha.hmza@gmail.com>
2024-05-25 17:50:09 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,49 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+declare var webkitSpeechRecognition: new () => any;
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
 
-if (environment.production) {
-  enableProdMode();
+
+    <button  (click)="startListening()">Click me</button>
+  `,
+})
+export class App {
+  name = 'Angular';
+
+  results: any;
+
+  startListening() {
+    // let voiceHandler = this.hiddenSearchHandler?.nativeElement;
+    if ('webkitSpeechRecognition' in window) {
+      const vSearch = new webkitSpeechRecognition();
+      vSearch.continuous = true;
+      vSearch.interimresults = false;
+      vSearch.lang = 'ar-SA';
+      vSearch.start();
+      vSearch.onresult = (e: { results: { transcript: any; }[][]; }) => {
+        console.log(e);
+        // voiceHandler.value = e?.results[0][0]?.transcript;
+        this.results = e.results[0][0].transcript;
+        this.getResult();
+        // console.log(this.results);
+        vSearch.stop();
+      };
+    } else {
+      alert('Your browser does not support voice recognition!');
+    }
+  }
+
+  getResult() {
+    console.log(this.results);
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(App);
